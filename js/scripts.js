69595ff025c95cb32a30700f6cd2cfca2a349bc5
murumba631 <m.arnold1738@gmail.com>
2020-07-19 07:03:16 +0300

    add parseInt to js file

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var dd = parseInt(document.getElementById("day"))
+var mm = parseInt(document.getElementById("month"))
+var cc = parseInt(document.getElementById("century"))
+var yy = parseint(document.getElementById("year"))
+
+    
