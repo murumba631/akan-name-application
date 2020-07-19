c3a5166b1b4e28191e8020f81982d70541aee2bb
murumba631 <m.arnold1738@gmail.com>
2020-07-19 14:39:46 +0300

    add variable for gender

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var dd = parseInt(document.getElementById("day"))
 var mm = parseInt(document.getElementById("month"))
 var cc = parseInt(document.getElementById("century"))
 var yy = parseint(document.getElementById("year"))
+var gender = document.getElementById("gender")
 
 var result=(( ( cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd) % 7;
 alert(result); 
