53f7ec10e3c225317e3f0585eab2ae1b8a47a2c5
murumba631 <m.arnold1738@gmail.com>
2020-07-19 07:27:34 +0300

    add formula

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,5 @@ var mm = parseInt(document.getElementById("month"))
 var cc = parseInt(document.getElementById("century"))
 var yy = parseint(document.getElementById("year"))
 
-    
+var result=(( ( cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd) % 7;
+alert(result); 
